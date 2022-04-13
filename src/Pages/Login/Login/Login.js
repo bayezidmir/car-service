47c63333056aa4e70ca5ebd59305bb3dbfda4bf9
bayezid mir <warner.roman@gmail.com>
2022-04-13 08:13:57 +0600

    added style to registration and login form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="warning" type="submit">
           Submit
         </Button>
         <p>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Register.css";
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="container mx-auto w-50">
       <h1>Register</h1>
-      <form className="registration-form">
+      <form className="registration-form" onSubmit={handleSubmit}>
         <input type="text" name="name" id="" placeholder="Your Name" />
         <input type="email" name="email" id="" placeholder="Your Email" />
         <input type="password" name="password" id="" placeholder="Password" />
@@ -17,6 +21,12 @@ const Register = () => {
           />
         </div>
       </form>
+      <p>
+        Already a member?{" "}
+        <Link to="/login" className="text-danger text-decoration-none">
+          Please Login
+        </Link>
+      </p>
     </div>
   );
 };
